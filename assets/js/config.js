1358bbffbf45a4032c23e8e4082832c4e23c641e
Buxta <robertpdavis@optusnet.com.au>
2022-03-14 00:02:54 +1000

    Further fucntionality added - results display and local storage

--- assets/js/config.js
+++ assets/js/config.js
@@ -4,14 +4,14 @@ var arrQuestions = `
         {"Q":"Javascript is the best language?",
         "1":"True",
         "2":"False",
-        "A":"True"}
+        "A":"1"}
     ,
         {"Q":"Which brackets are used to create an object in javascript?",
         "1":"[ ]",
         "2":"{ }",
         "3":"( )",
         "4":"| |",
-        "A":"{ }"}
+        "A":"2"}
 ]
 `
 //Levels object
@@ -24,4 +24,7 @@ var objLevels = {
 }
 
 //Time var for game - in seconds
-var setTime = 5;
+var setTime = 30;
+
+//Time penalty for wrong answer - in seconds
+var setTimePenalty = 5;
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,30 @@
 //Javascript Quizmaster code
 
-
+//Set variables
 var startButton = document.querySelector("#start-button");
 var timerDisplay = document.querySelector("#timer-display");
+var timeRemaining = document.querySelector("#time-remaining");
 var questionText = document.querySelector("#question-text");
 var answerList = document.querySelector("#answer-list");
-var timeLeft = setTime;
-var correct = 0;
-var wrong = 0;
+var resultText = document.querySelector("#result-text");
+var initialButton = document.querySelector("#initial-button");
+var initials = document.querySelector("#initials");
+var timer;
+var timeLeft;
+var correct;
+var wrong;
 var questionCount = 0;
-var questionNum = 0;
+var questionNum;
 
 
-//Listeners
+//Listeners---------------------------------------------------
 //Start button
 startButton.addEventListener("click", function(event) {
     event.preventDefault();
+    startButton.textContent === "Start Quiz";
+
+    startButton.disabled = true;
+
     startQuiz();
 });
 
@@ -23,12 +32,30 @@ startButton.addEventListener("click", function(event) {
 answerList.addEventListener("click", function(event) {
     event.preventDefault();
     //Check the answer and call the next question
-    checkAnswer();
+    checkAnswer(event);
+    displayQuestion();
 });
 
+//Initials submit
+initialButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    //Check the answer and call the next question
+    saveHighScore();
+});
+//-----------------------------------------------------------
 
+
+//Functions--------------------------------------------------
 
 function startQuiz () {
+    timeLeft = setTime;
+    correct = 0;
+    wrong = 0;
+    questionNum = 0;
+    
+
+
+    timeRemaining.textContent = "seconds remaining"
 
     //Shuffle questions so no in the same order for each quiz
     arrQuestions = shuffle(arrQuestions);
@@ -38,8 +65,8 @@ function startQuiz () {
 
     // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
     var timer = setInterval(function () {
-        // As long as the `timeLeft` is greater than 1
-        if (timeLeft > 0) {
+        // As long as the `timeLeft` is 1 second or more
+        if (timeLeft > 0 && questionNum < questionCount) {
 
             timeLeft--;
             timerDisplay.textContent = timeLeft;
@@ -49,55 +76,92 @@ function startQuiz () {
             clearInterval(timer);
             // Call the `displayResult()` function
             displayResult();
+
+            startButton.disabled = false;
+            startButton.textContent= "Redo Quiz";
+
         }
     }, 1000);
 
 }
 
 function displayQuestion() {
-    // Clear previous question answers
-    answerList.innerHTML = "";
+    if (timeLeft > 0 && questionNum < questionCount) {
+        // Clear previous question answers
+        answerList.innerHTML = "";
 
-    //Get question/answers from question array
-    question = arrQuestions[questionNum];
+        //Get question/answers from question array
+        question = arrQuestions[questionNum];
 
-    //Dispay question
-    questionText.textContent = question["Q"];
+        //Dispay question
+        questionText.textContent = question["Q"];
 
-    for (var key in question) {
-        if (Object.hasOwnProperty.call(question, key)) {
-            if (key != "Q" && key != "A") {         
-                var answer = key + "." + question[key];
-                var li = document.createElement("li");
-                li.setAttribute("data-index", 1);
+        for (var key in question) {
+            if (Object.hasOwnProperty.call(question, key)) {
+                if (key != "Q" && key != "A") {         
+                    var answer = key + "." + question[key];
+                    var li = document.createElement("li");
+                    li.setAttribute("data-index", 1);
 
-                var button = document.createElement("button");
-                button.textContent = answer;
-            
-                li.appendChild(button);
-                answerList.appendChild(li);
+                    var button = document.createElement("button");
+                    button.textContent = answer;
+                
+                    li.appendChild(button);
+                    answerList.appendChild(li);
+                }
             }
         }
     }
-    return;
 }
 
 //Check the answer selected - dislay result on screen and record result
-function checkAnswer() {
+function checkAnswer(event) {
+    if (timeLeft > 0 && questionNum < questionCount) {
+        var answer = arrQuestions[questionNum]["A"];
+        var userAnswer = event.target.textContent.charAt(0);
 
+        if (userAnswer === answer) {
+            correct++;
+            resultText.innerHTML= "<br><hr><br>Correct";
+
+        } else  {
+            wrong++;
+            timeLeft = timeLeft - setTimePenalty;
+            resultText.innerHTML = "<br><hr><br>Wrong";
+        }
+
+        //Dispay if answer was correct or wrong for period
+        setTimeout(function () {
+            resultText.innerHTML = "";       
+        },750);
+
+        questionNum++;
+    }
 }
 
 //Display quiz result on sreen
 function displayResult() {
-    timerDisplay.textContent ="";
-
+    timerDisplay.textContent = "Your Score: " + correct;
+    timeRemaining.textContent = "";
+    questionText.textContent = "Please enter your initials and submit for the highsscores list";
+    answerList.style.display = "none";
+    initials.style.display = "block";
+    initialButton.style.display= "block";
 }
 
-//Save reesult to high scores list
-function saveResult() {
-    
- // set new submission to local storage 
- localStorage.setItem("user", JSON.stringify(user));
+//Save result to high scores list and go to page
+function saveHighScore() {
+    var savedScores = JSON.parse(localStorage.getItem("highScores"));
+
+    if (savedScores===null) {
+        var highScores = {};
+    }
+    console.log(highScores);
+    console.log(initials.value);
+    highScores[initials.value] = correct;
+
+    // set new entry to local storage 
+    localStorage.setItem("highScores", JSON.stringify(highScores));
 }
 
 //Shuffle function using Fisher-Yates Shuffle - https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
@@ -116,11 +180,15 @@ function shuffle (items) {
 function init() {
     //Display timer start time
     timerDisplay.textContent = setTime;
-
     //Parse questions
     arrQuestions = JSON.parse(arrQuestions);
+    //Get number of questions
     questionCount = arrQuestions.length;
 
+    initials.style.display= "none";
+    initialButton.style.display= "none";
 }
+
+//-----------------------------------------------------------
 
 init();
